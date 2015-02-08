e90849bda55ea2ccbe08681a287751e3eb030373
Will Clarkson <william.w.clarkson@gmail.com>
2015-02-08 14:55:41 -0500

    cleaned up closed websocket connections so they can be reopened successfully

--- appinventor/components/src/com/google/appinventor/components/runtime/BlockyTalky.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/BlockyTalky.java
@@ -143,7 +143,7 @@ public class BlockyTalky extends AndroidNonvisibleComponent implements Component
         private String destination;
         private String content;
         private String jsonFormatString =
-                "{\"py/object\": \"__main__.Message\", \"content\": \"%s\", \"destination\": \"%s\", \"source\": \"%s\"}";
+                "{\"py/object\": \"__main__.Message\", \"channel\": \"Message\", \"content\": \"%s\", \"destination\": \"%s\", \"source\": \"%s\"}";
 
         public BlockyTalkyMessage(String source, String destination, String content) {
             this.source = source;
@@ -192,6 +192,7 @@ public class BlockyTalky extends AndroidNonvisibleComponent implements Component
         @Override
         public void onClose(int code, String reason, boolean remote) {
             Log.d(LOG_TAG, "closed with exit code " + code + " additional info: " + reason);
+            clients.values().remove(this);
         }
 
         @Override
